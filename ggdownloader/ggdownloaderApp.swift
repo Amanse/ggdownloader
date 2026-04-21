e910ea2f7cdca86730482fa425a1a268d3aaf4b9
Aman Setia <amansetiarjp@gmail.com>
2026-04-22 00:34:59 +0530

    Initial Commit

---
+++ ggdownloader/ggdownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ggdownloaderApp.swift
+//  ggdownloader
+//
+//  Created by Aman Setia on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ggdownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
